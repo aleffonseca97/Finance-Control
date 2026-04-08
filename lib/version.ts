06062598ceebb240b72dd151abbef807ad2e871f
Henrique Augusto <henrimus.96@hotmail.com>
2026-04-08 15:10:24 -0300

    Update application version to 1.2-beta.1

--- lib/version.ts
+++ lib/version.ts
@@ -1,3 +1,3 @@
 // Versão da aplicação - mantenha em sincronia com package.json
-export const APP_VERSION = '0.7.0';
+export const APP_VERSION = '1.2-beta.1';
 export const APP_NAME = 'Logos Finance';

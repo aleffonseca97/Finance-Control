dc9ad41eac3723011accfbbabafe7f369cb90f0d
henri-augusto <henrimus.96@hotmail.com>
2026-04-09 17:54:24 -0300

    app version

--- lib/version.ts
+++ lib/version.ts
@@ -1,3 +1,3 @@
 // Versão da aplicação - mantenha em sincronia com package.json
-export const APP_VERSION = '0.7.0';
+export const APP_VERSION = '1.3.1-beta';
 export const APP_NAME = 'Logos Finance';
